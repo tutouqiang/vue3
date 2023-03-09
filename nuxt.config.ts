e01b7c35ef4d1db95778df4ac806f2ac8332f0f0
wooc <wooc@cz-robots.com>
2023-03-09 19:07:44 +0800

    feat: 1.add doc; 2. perf page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Home',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'A front-end developer who loves code。' },
+        { name: 'keywords', content: 'wooc, Wooc, WOOC, zhangchao, zhangchao-wooc' }
+      ],
+    }
+  },
   modules: [
     '@nuxt/content'
   ],
